cae6ba0a3831de6eea8c3403b5b132c78d81346f
Airat Safiullin <atsafiullin@gmail.com>
2017-09-07 18:08:22 +0300

    init libraries

--- GithubClient/app/src/main/java/github/maxat/com/githubclient/MainActivity.java
+++ GithubClient/app/src/main/java/github/maxat/com/githubclient/MainActivity.java
@@ -2,12 +2,24 @@ package github.maxat.com.githubclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+
+	@BindView (R.id.tvTitle)
+	TextView tvTitle;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate (savedInstanceState);
 		setContentView (R.layout.activity_main);
+
+		ButterKnife.bind (this);
+
+		tvTitle.setText ("Hello, Android applicaiton");
 	}
 }
---
+++ GithubClient/app/src/main/java/github/maxat/com/githubclient/presentation/AndroidApplication.java
@@ -0,0 +1,15 @@
+package github.maxat.com.githubclient.presentation;
+
+import android.app.Application;
+
+/**
+ * Created by ayrat on 07.09.17.
+ */
+public class AndroidApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate ();
+	}
+
+}
